c7da928f4c3489ecb34cf6da5415df19ecace828
DeepikaGhodki <deepikaghodki@gmail.com>
2022-07-27 16:23:47 -0400

    Added views that do nothing

--- myjournal/notes/urls.py
+++ myjournal/notes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("book/<int:notebook_id>/", views.notebook, name="notebook"),
+    path("<int:note_id>/", views.note, name="note"),
 ]
--- myjournal/notes/views.py
+++ myjournal/notes/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're reading my notes")
+
+
+def notebook(request, notebook_id):
+    return HttpResponse("This is a notebook")
+
+
+def note(request, note_id):
+    return HttpResponse("This is a note")
