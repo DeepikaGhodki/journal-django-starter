b4e3d848c4e35859209c885bd6ff44a227de5e2e
DeepikaGhodki <deepikaghodki@gmail.com>
2022-07-28 02:34:22 -0400

    Added views

--- myjournal/notes/views.py
+++ myjournal/notes/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Notebook, Note
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're reading my notes")
+    notebooks = Notebook.objects.all()
+    context = {"notebooks": notebooks}
+    # notes/template/notes/index.html as notes/index.html => django format
+    return render(request, "notes/index.html", context)  # returns httpresponse
 
 
 def notebook(request, notebook_id):
-    return HttpResponse("This is a notebook")
+    try:
+        notebook = Notebook.objects.get(pk=notebook_id)
+    except Notebook.DoesNotExist:
+        raise Http404("Notebook does not exist")
+    return render(request, "notes/notebook.html", {"notebook": notebook})
 
 
 def note(request, note_id):
-    return HttpResponse("This is a note")
+    note = get_object_or_404(Note, pk=note_id)
+    return render(request, "notes/note.html", {"note": note})
